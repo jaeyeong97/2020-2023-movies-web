88d230d9889fc815a793028e7e5566c3943f7843
jaeyeong97 <rkaakdbdb1822@naver.com>
2024-05-18 13:45:22 +0900

    리뷰 로컬 스토리지 저장

--- src/component/MovieModal.js
+++ src/component/MovieModal.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import MovieReview from "./MovieReview";
 
 const MovieModal = ({ closeModal, id, age, title, rating, length, photo, director, actors, summary, checkRating }) => {
@@ -6,7 +6,19 @@ const MovieModal = ({ closeModal, id, age, title, rating, length, photo, directo
     const [review, setReview] = useState('');
     const [reviewData, setReviewData] = useState([])
     const [points, setPoints] = useState('10점')
-    
+
+    // 로컬 스토리지에서 불러오기
+    useEffect(() => {
+        const rv = JSON.parse(localStorage.getItem(`rv_${id}`)) || '';
+        setReviewData(rv);
+        dataId.current = rv.length ? rv[0].id + 1 : 0; // dataId를 다음 id로 조정
+    }, [id]);
+
+    // 로컬 스토리지에 리뷰 저장
+    useEffect(() => {
+        localStorage.setItem(`rv_${id}`, JSON.stringify(reviewData));
+    }, [reviewData, id]);
+
     const handlePoints = e => {
         setPoints(e.target.value)
     }
@@ -17,16 +29,16 @@ const MovieModal = ({ closeModal, id, age, title, rating, length, photo, directo
     const createReview = (content, points) => {
         const createdDate = new Date().getTime();
         const newReview = {
-            id : dataId.current,
-            content : content,
-            createdDate : createdDate,
-            points : points
+            id: dataId.current,
+            content: content,
+            createdDate: createdDate,
+            points: points
         }
         dataId.current += 1;
         setReviewData([newReview, ...reviewData])
     }
     const handleSaveReview = () => {
-        if(review.length < 10) {
+        if (review.length < 10) {
             window.alert('10글자 이상 작성하셔야 등록됩니다.')
             return;
         }
@@ -35,30 +47,30 @@ const MovieModal = ({ closeModal, id, age, title, rating, length, photo, directo
         setPoints('10점')
     }
     const checkPoints = point => {
-        if(point === '10점') {
+        if (point === '10점') {
             return '⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐';
-        } else if(point === '9점') {
+        } else if (point === '9점') {
             return '⭐⭐⭐⭐⭐⭐⭐⭐⭐';
-        } else if(point === '8점') {
+        } else if (point === '8점') {
             return '⭐⭐⭐⭐⭐⭐⭐⭐';
-        } else if(point === '7점') {
+        } else if (point === '7점') {
             return '⭐⭐⭐⭐⭐⭐⭐';
-        } else if(point === '6점') {
+        } else if (point === '6점') {
             return '⭐⭐⭐⭐⭐⭐';
-        } else if(point === '5점') {
+        } else if (point === '5점') {
             return '⭐⭐⭐⭐⭐';
-        } else if(point === '4점') {
+        } else if (point === '4점') {
             return '⭐⭐⭐⭐';
-        } else if(point === '3점') {
+        } else if (point === '3점') {
             return '⭐⭐⭐';
-        } else if(point === '2점') {
+        } else if (point === '2점') {
             return '⭐⭐';
-        } else if(point === '1점') {
+        } else if (point === '1점') {
             return '⭐';
         }
     }
     const enterKeyDown = (e) => {
-        if(e.keyCode === 13) {
+        if (e.keyCode === 13) {
             handleSaveReview()
         }
     }
@@ -78,14 +90,14 @@ const MovieModal = ({ closeModal, id, age, title, rating, length, photo, directo
                     <span className="title">{title}</span>
                     <div className="arl">
                         <span className="age">{age}</span>
-                        <span className="rating">평점 : {rating} {checkRating(rating)}</span>
+                        <span className="rating">평점: {rating} {checkRating(rating)}</span>
                         <span className="length">{length}</span>
                     </div>
                     <span className="summary">{summary}</span>
                     <span className="director">감독 : {director}</span>
                     <span className="actors">출연배우 : {actors}</span>
                     <div className="review">
-                        <input 
+                        <input
                             type="text"
                             value={review}
                             onChange={handleReview}
@@ -111,7 +123,7 @@ const MovieModal = ({ closeModal, id, age, title, rating, length, photo, directo
                     </div>
                     <ul>
                         {reviewData.map((it) => (
-                            <MovieReview {...it} key={it.id} checkPoints={checkPoints}/>
+                            <MovieReview {...it} key={it.id} checkPoints={checkPoints} />
                         ))}
                     </ul>
                 </div>
--- src/component/MovieReview.js
+++ src/component/MovieReview.js
@@ -1,9 +1,11 @@
-const MovieReview = ({id, content, createdDate, points, checkPoints}) => {
+const MovieReview = ({ content, createdDate, points, checkPoints }) => {
     return (
         <li className="MovieReview">
-            <span>관람객 리뷰 : {content}</span>
-            <span>{new Date(createdDate).toLocaleString()}</span>
-            <span>{points} {checkPoints(points)}</span>
+            <div className="review_txt"><span>관람객 리뷰</span>: {content}</div>
+            <div className="points_date_wrap">
+                <span className="review_points">{points} {checkPoints(points)}</span>
+                <span className="review_date">{new Date(createdDate).toLocaleString()}</span>
+            </div>
         </li>
     );
 }
